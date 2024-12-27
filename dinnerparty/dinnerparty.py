32ce8b988c5816964a9def133565b5568017b838
Mark <boiko@student.khai.edu>
2024-12-27 23:41:15 +0200

    1st

--- dinnerparty/dinnerparty.py
+++ dinnerparty/dinnerparty.py
@@ -1,21 +1,16 @@
-import random
-# stage 1
-number_of_friends = int(input("Enter the number of friends joining (including you):\n"))
+#1st
 
-if number_of_friends <= 0:
+num_friends = int(input("Enter the number of friends joining (including you): "))
+
+if num_friends <= 0:
     print("No one is joining for the party")
 else:
-    friends = {}
+    friends_dict = {}
+
     print("Enter the name of every friend (including you), each on a new line:")
-    for _ in range(number_of_friends):
-        name = input("> ")
-        friends[name] = 0
 
-# stage 2
-    total_amount = float(input("Enter the total amount:\n"))  # Загальна сума
-    split_amount = round(total_amount / number_of_friends, 2)  # Сума для кожного друга
+    for _ in range(num_friends):
+        name = input()
+        friends_dict[name] = 0
 
-for name in friends:
-    friends[name] = split_amount
-
-print(friends)
+    print(friends_dict)
