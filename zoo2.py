e4b41a8f26b421bac428cb549790e042843b3c64
Mark <boiko@student.khai.edu>
2024-12-27 17:04:41 +0200

    zoo 2-nd stage

--- zoo2.py
+++ zoo2.py
@@ -25,6 +25,20 @@ ___.-''''-.
          ; ;   !  !  ! ; ; ;
          ;,,   !,,!,,! ;,;
           /_I   L_I   L_I
-Look at that!"""
+Look at that!
+"""
 
 print(camel)
+
+# 3-rd stage
+animals = [camel]
+
+while True:
+    user_input = input("Please enter the number of the habitat you would like to view: > ").strip()
+    if user_input.lower() == "exit":
+        print("See you later!")
+        break
+    elif user_input.isdigit() and int(user_input) == 0:
+        print(animals[0])
+    else:
+        print("Invalid input. Please enter 0 to view the camel or 'exit' to quit.")
