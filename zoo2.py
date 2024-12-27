1cdb4f5e63636ea365eb32160fd69d05b84c2ba8
Mark <boiko@student.khai.edu>
2024-12-27 16:51:24 +0200

    zoo2 2-nd stage

--- zoo2.py
+++ zoo2.py
@@ -1,5 +1,29 @@
-# stage 1
+# 1-st stage
 print("I love animals!")
 print("Let's check out the animals...")
 print("The deer looks fine.")
 print("\nThe lion looks healthy.")
+
+# 2-nd stage
+camel = r"""
+The camel habitat...
+___.-''''-.
+/___ @ |
+',,,,. | _.'''''''._
+' | / \
+| \ _.-' \
+| '.-' '-.
+| ',
+| '',
+',,-, ':;
+',,| ;,, ,' ;;
+! ; !'',,,',',,,,'! ; ;:
+: ; ! ! ! ! ; ; :;
+; ; ! ! ! ! ; ; ;,
+; ; ! ! ! ! ; ;
+; ; ! ! ! ! ; ;
+;,, !,! !,! ;,;
+/_I L_I L_I /_I
+Look at that!"""
+
+print(camel)
