82a6e4b8b0a010993334b1484d6e95809a63e63e
Mark <boiko@student.khai.edu>
2024-12-27 04:40:27 +0200

    zoo 3-rd stage

--- zoo.py
+++ zoo.py
@@ -1,4 +1,27 @@
+# 1-st stage
 print("I love animals!")
 print("Let's check out the animals...")
 print("The deer looks fine.")
 print("The lion looks healthy.")
+camel = r"""
+The camel habitat...
+___.-''''-.
+/___ @ |
+',,,,. | _.'''''''._
+' | / \
+| \ _.-' \
+| '.-' '-.
+| ',
+| '',
+',,-, ':;
+',,| ;,, ,' ;;
+! ; !'',,,',',,,,'! ; ;:
+: ; ! ! ! ! ; ; :;
+; ; ! ! ! ! ; ; ;,
+; ; ! ! ! ! ; ;
+; ; ! ! ! ! ; ;
+;,, !,! !,! ;,;
+/_I L_I L_I /_I
+Look at that!
+"""
+print(camel)
