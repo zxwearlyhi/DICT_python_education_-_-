e70e72f9f3ff0311f0f8d302d3017ea9116009bc
Mark <boiko@student.khai.edu>
2024-12-27 04:41:24 +0200

    zoo 4-th stage

--- zoo.py
+++ zoo.py
@@ -3,6 +3,7 @@ print("I love animals!")
 print("Let's check out the animals...")
 print("The deer looks fine.")
 print("The lion looks healthy.")
+
 camel = r"""
 The camel habitat...
 ___.-''''-.
@@ -25,3 +26,13 @@ ___.-''''-.
 Look at that!
 """
 print(camel)
+
+animals = [camel, lion, deer, goose, bat, rabbit]
+
+# запит номеру тварини
+animal_number = int(input("Please enter the number of the habitat you would like to view: "))
+
+# виведення відповідного зображення
+print(animals[animal_number - 1])
+print("You've reached the end of the program.")
+
