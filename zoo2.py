f6f747cba6a411472743c36a590f494e40075c14
Mark <boiko@student.khai.edu>
2024-12-27 16:53:02 +0200

    zoo2 3-rd stage

--- zoo2.py
+++ zoo2.py
@@ -27,3 +27,36 @@ ___.-''''-.
 Look at that!"""
 
 print(camel)
+
+# 3-rd stage
+camel = r"""
+The camel habitat...
+___.-''''-.
+/___  @      |
+',,,,.   |  _.'''''''._
+     '    | /          
+     |    |              \ 
+     |     \     _.-'  '-.
+     |      '.-'         '.
+     |       ',          |
+     |        '',        |
+      ',,,-,   ':;       '
+         ',,| ;,,       ,' ;;
+            ! ; !,,,,,,',,; ;:
+           : ; !  !  ! ; ; :
+           ; ; !  !  ! ; ; ;,
+          ; ;  !  !  ! ; ; ;
+         ; ;   !  !  ! ; ; ;
+         ;,,   !,,!,,! ;,;
+          /_I   L_I   L_I
+Look at that!
+"""
+
+animals = [camel]
+
+user_input = input("Please enter the number of the habitat you would like to view: > ").strip()
+
+if user_input.isdigit() and int(user_input) == 0:
+    print(animals[0])
+else:
+    print("You've reached the end of the program.")
