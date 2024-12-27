b7f63cb98dedc0981ad4433c448c303333b62acd
Mark <boiko@student.khai.edu>
2024-12-27 20:27:42 +0200

    2-nd stage

--- hangman.py
+++ hangman.py
@@ -6,3 +6,21 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+    # 2-nd stage
+
+    def main():
+        print("HANGMAN")
+
+        word = "python"  # Задано слово
+        guess = input("Guess the word: > ")  # Запросить ввод от пользователя
+
+        if guess.lower() == word:
+            print("You survived!")  # Пользователь угадал слово
+        else:
+            print("You lost!")  # Пользователь не угадал слово
+
+
+    if __name__ == "__main__":
+        main()
