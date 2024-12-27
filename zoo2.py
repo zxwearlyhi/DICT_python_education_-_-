e446278f5d6328b5a689dbee9aab87867017563f
Mark <boiko@student.khai.edu>
2024-12-27 16:57:43 +0200

    zoo2 2-nd stage

--- zoo2.py
+++ zoo2.py
@@ -8,54 +8,23 @@ print("\nThe lion looks healthy.")
 camel = r"""
 The camel habitat...
 ___.-''''-.
-/___ @ |
-',,,,. | _.'''''''._
-' | / \
-| \ _.-' \
-| '.-' '-.
-| ',
-| '',
-',,-, ':;
-',,| ;,, ,' ;;
-! ; !'',,,',',,,,'! ; ;:
-: ; ! ! ! ! ; ; :;
-; ; ! ! ! ! ; ; ;,
-; ; ! ! ! ! ; ;
-; ; ! ! ! ! ; ;
-;,, !,! !,! ;,;
-/_I L_I L_I /_I
+/___  @      |
+',,,,.   |  _.'''''''._
+     '    | /          
+     |    |              \ 
+     |     \     _.-'  '-.
+     |      '.-'         '.
+     |       ',          |
+     |        '',        |
+      ',,,-,   ':;       '
+         ',,| ;,,       ,' ;;
+            ! ; !,,,,,,',,; ;:
+           : ; !  !  ! ; ; :
+           ; ; !  !  ! ; ; ;,
+          ; ;  !  !  ! ; ; ;
+         ; ;   !  !  ! ; ; ;
+         ;,,   !,,!,,! ;,;
+          /_I   L_I   L_I
 Look at that!"""
 
 print(camel)
-
-# 3-rd stage
-camel = r"""
-The camel habitat...
-___.-''''-.
-/___ @ |
-',,,,. | _.'''''''._
-' | / \
-| \ _.-' \
-| '.-' '-.
-| ',
-| '',
-',,-, ':;
-',,| ;,, ,' ;;
-! ; !'',,,',',,,,'! ; ;:
-: ; ! ! ! ! ; ; :;
-; ; ! ! ! ! ; ; ;,
-; ; ! ! ! ! ; ;
-; ; ! ! ! ! ; ;
-;,, !,! !,! ;,;
-/_I L_I L_I /_I
-Look at that!
-"""
-
-animals = [camel]
-
-user_input = input("Please enter the number of the habitat you would like to view: > ").strip()
-
-if user_input.isdigit() and int(user_input) == 0:
-    print(animals[0])
-else:
-    print("You've reached the end of the program.")
