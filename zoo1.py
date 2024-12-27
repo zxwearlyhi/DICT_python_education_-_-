289a0d5d938cd240e53bacd5d214d0afd893784e
Mark <boiko@student.khai.edu>
2024-12-27 16:34:59 +0200

    Ñzoo1 2-nd stage

--- zoo1.py
+++ zoo1.py
@@ -3,3 +3,29 @@ print("I love animals!")
 print("Let's check out the animals...")
 print("The deer looks fine.")
 print("\nThe lion looks healthy.")
+
+# stage 2
+camel = r"""
+The camel habitat...
+___.-''''-.
+/___  @      |
+',,,,.   |  _.'''''''._
+     '    | /          
+     |    |              \ 
+     |     \     _.-'  '-.
+     |      '.-'         '.
+     |       ',          |
+     |        '',        |
+      ',,,-,   ':;       '
+         ',,| ;,,       ,' ;;
+            ! ; !,,,,,,',,; ;:
+           : ; !  !  ! ; ; :
+           ; ; !  !  ! ; ; ;,
+          ; ;  !  !  ! ; ; ;
+         ; ;   !  !  ! ; ; ;
+         ;,,   !,,!,,! ;,;
+          /_I   L_I   L_I
+Look at that!
+"""
+
+print(camel)
